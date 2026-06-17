47b5eb678d0991bd97c200f4e4cc27ff72be0a0e
aryaakalpavriksha1-max <aryaakalpavriksha1@gmail.com>
2026-06-17 07:38:01 +0530

    Update frozen_lake_agent.py

--- frozen_lake_agent.py
+++ frozen_lake_agent.py
@@ -4,12 +4,12 @@ import numpy as np
 def train_agent():
     # Initialize environment (is_slippery=False makes it deterministic for easier learning)
     env = gym.make("FrozenLake-v1", is_slippery=False)
-    
+
     # Hyperparameters (TRY TWEAKING THEM!!!)
     num_episodes = 10000
-    alpha = 0.8       # Learning rate
-    gamma = 0.95      # Discount factor
-    epsilon = 1.0     # Exploration rate
+    alpha = 0.8
+    gamma = 0.95
+    epsilon = 1.0
     min_epsilon = 0.01
     epsilon_decay = 0.995
 
@@ -19,20 +19,19 @@ def train_agent():
     for i in range(num_episodes):
         state, _ = env.reset()
         done = False
-        
+
         while not done:
             # ==========================================
             # TODO 1: Implement Epsilon-Greedy Action Selection
-            # Hint: Generate a random number using np.random.uniform(0, 1). 
+            # Hint: Generate a random number using np.random.uniform(0, 1).
             # If it's less than epsilon, choose a random action (env.action_space.sample()).
             # Otherwise, choose the action with the highest Q-value for the current state (np.argmax).
             # ==========================================
-           if np.random.uniform(0, 1) < epsilon:
+            if np.random.uniform(0, 1) < epsilon:
                 action = env.action_space.sample()  # Explore
             else:
                 action = np.argmax(q_table[state, :])  # Exploit
-            
-            
+
             # Take the action
             next_state, reward, terminated, truncated, _ = env.step(action)
             done = terminated or truncated
@@ -43,14 +42,12 @@ def train_agent():
             # ==========================================
             best_next_action = np.max(q_table[next_state, :])
             td_target = reward + gamma * best_next_action
-            
+
             # Update the Q-Table using the TD Error
             q_table[state, action] += alpha * (td_target - q_table[state, action])
-            
+
             state = next_state
-            
-            
-            
+
         # Decay exploration rate
         epsilon = max(min_epsilon, epsilon * epsilon_decay)
 
